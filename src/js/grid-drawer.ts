168a1cf23977fc88988c1a21adaee305841112e5
丘惟夫 <waveciou@gmail.com>
2021-04-10 22:01:27 +0800

    refactor with typescript

---
+++ src/js/grid-drawer.ts
@@ -0,0 +1,5 @@
+import '../scss/grid-drawer.scss';
+
+let a: string = '安安';
+const aaa: string = `你好${a}`;
+console.log(aaa);
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 環境變數
+console.log(process.env.NODE_ENV);
+
+module.exports = {
+  entry: {
+    'grid-drawer': path.resolve(__dirname, './src/js/grid-drawer.ts')
+  },
+  output: {
+    filename: './js/[name].min.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(scss|sass)$/i,
+        include: path.resolve('./src/scss'),
+        exclude: path.resolve('./node_modules'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { 
+              publicPath: '../',
+            }
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['autoprefixer']
+                ]
+              }
+            }
+          },
+          'sass-loader'
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: './css/[name].min.css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: false,
+      inject: false
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/img',
+          to: './img'
+        }
+      ]
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    port: 3000,
+    stats: {
+      assets: true,
+      cached: false,
+      chunkModules: false,
+      chunkOrigins: false,
+      chunks: false,
+      colors: true,
+      hash: false,
+      modules: false,
+      reasons: false,
+      source: false,
+      version: false,
+      warnings: false
+    }
+  }
+};
